e48969a9425088a8275ba806f4d4e194f604f10d
LAIEB Rachid <rachid.laieb@gmail.com>
2022-10-10 10:08:28 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let jeuTableau;
+let allCards = document.querySelectorAll("card");
+allCards.forEach((card) => {
+  card.addEventListener("click", function () {
+    if (card.classList.contains("caché")) {
+      card.classList.remove("caché");
+    } else {
+      card.classList.add("caché");
+    }
+  });
+});
+
+// function generateGameArray(x);
+// let Tableau = new Array(x);
+// for (var i = 0; i< Tableau.length; i++) {
+//     Tableau[i] = new Array(y);
+// }
+
+// jeuTableau = Tableau;
+
+// function getRamdomInt(max) {
+//     return Math.floor(Math.random() * max)
+// };
